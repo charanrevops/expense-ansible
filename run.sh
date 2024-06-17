b1ccef4d0bf0aa631a856e2311589463ce00ed4f
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-17 12:34:05 +0200

    GoCD Template

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+# for template pipeline we will skip the run
+
+if [-z "$pipeline" == "true"]; then
+  exit
+fi
+
 ansible-playbook -i ${component}-${env}.charanrdevops.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
