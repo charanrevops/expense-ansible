4a366b399fcbc6b4dc4da7ac8e3051dbe65f4396
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-17 11:27:08 +0200

    Password

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i ${component}_${env}.charanrdevops.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
+ansible-playbook -i ${component}-${env}.charanrdevops.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
