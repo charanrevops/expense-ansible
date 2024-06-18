773de68b96c76707ad331c1db4ceb3dbf3cc4ab7
Charan Reddy <kothacharanreddy@gmail.com>
2024-06-18 16:40:12 +0200

    GoCD Template

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ if [ "$pipeline" == "true" ]; then
   exit
 fi
 
-#ansible-playbook -i ${component}-${env}.charanrdevops.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
-ansible --version
+ansible-playbook -i ${component}-${env}.charanrdevops.online, -e ansible_user=ec2-user -e ansible_password=${ssh_password} -e role_name=${component} -e env=dev expense.yml
+#ansible --version
